56afecae3d18a10c7770e0b899088986b704348f
prat0519 <prat0519@gmail.com>
2018-08-06 04:03:42 +0530

    Added CacheAnything Framework

---
+++ CacheAnything.framework/Headers/CacheAnything.h
@@ -0,0 +1,19 @@
+//
+//  CacheAnything.h
+//  CacheAnything
+//
+//  Created by Prashant Pandey on 05/08/18.
+//  Copyright © 2018 Prashant Pandey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CacheAnything.
+FOUNDATION_EXPORT double CacheAnythingVersionNumber;
+
+//! Project version string for CacheAnything.
+FOUNDATION_EXPORT const unsigned char CacheAnythingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CacheAnything/PublicHeader.h>
+
+
